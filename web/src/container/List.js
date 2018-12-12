d8918f2f371ddee7985659920cddf3f5790b7963
Yuki Hirai <yukihirai0505@gmail.com>
2018-12-12 17:36:34 +0900

    Update timeline design

--- web/src/container/List.js
+++ web/src/container/List.js
@@ -4,6 +4,7 @@ import { getListMembers, addListMember, removeListMember } from '../utils/api'
 import { Client } from 'dsteem'
 import { Cookie } from '../utils/cookie'
 import { api } from '../config/app'
+import ReactMarkdown from 'react-markdown'
 
 const client = new Client('https://api.steemit.com')
 
@@ -50,6 +51,9 @@ class List extends Component {
 
   async fetchTimeline() {
     const { members } = this.state
+    this.setState({
+      timeline: []
+    })
     members.forEach(async member => {
       const query = {
         tag: member[1].replace('@', ''),
@@ -87,12 +91,11 @@ class List extends Component {
 
   render() {
     const { username, members, newMemberName, timeline } = this.state
-    const { match } = this.props
+    const { history, match } = this.props
     const { name } = match.params
     const isMe = name === username
     return (
       <main>
-        <p>List Members</p>
         {isMe && (
           <form action="#" onSubmit={e => this.addListMember(e)}>
             Add Member: @
@@ -101,25 +104,27 @@ class List extends Component {
               value={newMemberName}
               onChange={e => this.handleMemberNameChange(e)}
             />
-            <input type="submit" value="create"/>
+            <input type="submit" value="add"/>
           </form>
         )}
-        <ul>
-          {members.map((member, key) => {
-            const listId = member[0]
-            const username = member[1]
-            return (
-              <li key={key}>
-                {username}{' '}
-                {isMe && (
-                  <a onClick={() => this.removeListMember(username)}>x</a>
-                )}
-              </li>
-            )
-          })}
-        </ul>
-        <p>Timeline</p>
+        {members.map((member, key) => {
+          const listId = member[0]
+          const username = member[1]
+          return (
+            <span key={key}>
+                {' '}{username}{' '}
+              {isMe && (
+                [<a onClick={() => this.removeListMember(username)}>x</a>]
+              )}
+              </span>
+          )
+        })}
         <div className='content-main'>
+          <div className='content-header'>
+            <div className='header-inner'>
+              <h2 id="content-main-heading">Timeline</h2>
+            </div>
+          </div>
           <div className='stream-container'>
             <div className='stream'>
               <ol className='stream-items'>
@@ -127,10 +132,9 @@ class List extends Component {
                   const json = JSON.parse(post.json_metadata)
                   const image = json.image ? json.image[0] : ''
                   console.log(post)
-                  console.log(json)
                   return (
                     <li key={key} className='stream-item'>
-                      <div className='tweet'>
+                      <div className='post'>
                         <div className='context'/>
                         <div className='content'>
                           <div className='stream-item-header'>
@@ -143,11 +147,45 @@ class List extends Component {
                             </a>
                             <div className='time'>{new Date(post.created).toDateString()}</div>
                           </div>
-                          <div>
-                            <p>{post.title}</p>
+                          <div
+                            onClick={() => window.open(`https://steemit.com${post.url}`)}>
+                            <div className='title'>{post.title}</div>
+                            <ReactMarkdown source={`${post.body.slice(0, 500)}...`} skipHtml={true}
+                                           disallowedTypes={['image', 'link', 'heading', 'strong', 'break']}
+                                           unwrapDisallowed={true}/>
                             <div className='media-container'>
                               <div className='adaptive-media is-square'>
                                 <img src={image} alt=""/>
+                              </div>
+                            </div>
+                          </div>
+                          <div className='stream-item-footer'>
+                            <div className='post-action-list'>
+                              <div className='post-action'>
+                                <button className='post-action-btn'>
+                                  <span
+                                    className='post-action-count'>
+                                    $ {(roundSBD(post.curator_payout_value) + roundSBD(post.total_payout_value) + roundSBD(post.pending_payout_value)).toFixed(2)}
+                                  </span>
+                                </button>
+                              </div>
+                              <div className='post-action'>
+                                <button className='post-action-btn'>
+                                  <i className="fa fa-chevron-up"/>
+                                  <span
+                                    className='post-action-count'>
+                                    {post.active_votes.length}
+                                  </span>
+                                </button>
+                              </div>
+                              <div className='post-action'>
+                                <button className='post-action-btn'>
+                                  <i className="fa fa-comments"/>
+                                  <span
+                                    className='post-action-count'>
+                                    {post.replies.length}
+                                  </span>
+                                </button>
                               </div>
                             </div>
                           </div>
@@ -163,6 +201,10 @@ class List extends Component {
       </main>
     )
   }
+}
+
+function roundSBD(value) {
+  return (Math.round(value.replace('SBD', '') * 100) / 100)
 }
 
 export default withRouter(List)
