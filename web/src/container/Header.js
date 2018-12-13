511dda5c85e4d56300d3fc48b53c7dcfc7b58384
Yuki Hirai <yukihirai0505@gmail.com>
2018-12-13 09:55:39 +0900

    Fix home btn link

--- web/src/container/Header.js
+++ web/src/container/Header.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import { withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import { productBaseUrl } from '../config/app'
 
 class Header extends Component {
   static propTypes = {
@@ -20,7 +21,7 @@ class Header extends Component {
               <div role='navigation' style={{ display: 'inline-block' }}>
                 <ul className='nav' id='global-actions'>
                   <li id="global-nav-home" className="home active new" data-global-action="home">
-                    <a className="js-nav js-tooltip js-dynamic-tooltip" data-placement="bottom" href="/">
+                    <a className="js-nav js-tooltip js-dynamic-tooltip" data-placement="bottom" href={productBaseUrl}>
                       <i className="fa fa-home"/>
                       <span className="text" aria-hidden="true">Home</span>
                     </a>
